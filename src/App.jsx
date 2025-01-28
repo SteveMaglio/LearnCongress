ad908b9d622c3328a8a713ae5e6c26d746eb6d64
SteveMaglio <magliocchino.s@northeastern.edu>
2025-01-27 20:12:29 -0500

    first commit using vite to set up react project

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const App = () => {
+  return (
+    <div style={{ textAlign: "center", marginTop: "20%" }}>
+      <h1>Hello World!</h1>
+      <p>Your React app is running successfully on localhost.</p>
+    </div>
+  );
+};
+
+export default App;
